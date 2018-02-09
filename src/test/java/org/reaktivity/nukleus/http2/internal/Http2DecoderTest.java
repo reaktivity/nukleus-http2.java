74489a1db2598f39c0560a79c0becbc4053c40e5
Jitendra Kotamraju <jitsni@gmail.com>
2018-02-09 14:39:27 -0800

    Zero copy changes

--- src/test/java/org/reaktivity/nukleus/http2/internal/Http2DecoderTest.java
+++ src/test/java/org/reaktivity/nukleus/http2/internal/Http2DecoderTest.java
@@ -24,7 +24,6 @@ import org.reaktivity.nukleus.buffer.MemoryManager;
 import org.reaktivity.nukleus.http2.internal.types.ListFW;
 import org.reaktivity.nukleus.http2.internal.types.stream.Http2DataFW;
 import org.reaktivity.nukleus.http2.internal.types.stream.Http2FrameFW;
-import org.reaktivity.nukleus.http2.internal.types.stream.Http2FrameType;
 import org.reaktivity.nukleus.http2.internal.types.stream.Http2HeadersFW;
 import org.reaktivity.nukleus.http2.internal.types.stream.Http2SettingsFW;
 import org.reaktivity.nukleus.http2.internal.types.stream.RegionFW;
@@ -45,24 +44,126 @@ import static org.reaktivity.nukleus.http2.internal.types.stream.Http2FrameType.
 import static org.reaktivity.nukleus.http2.internal.types.stream.Http2FrameType.SETTINGS;
 import static org.reaktivity.nukleus.http2.internal.types.stream.Http2PrefaceFW.PRI_REQUEST;
 
-public class Http2DecoderTest {
+public class Http2DecoderTest
+{
 
     private int frameCount = 0;
+
+    @Test
+    public void decodeSimple()
+    {
+        MemoryManager memoryManager = memoryManager();
+        MutableDirectBuffer buf = new UnsafeBuffer(new byte[0]);
+        long addressOffset = memoryManager.acquire(32768);
+        long resolvedOffset = memoryManager.resolve(addressOffset);
+        buf.wrap(resolvedOffset, 32768);
+
+        // PRI
+        int offset = 10;            // non-zero offset
+        buf.putBytes(offset, PRI_REQUEST);
+        Region r1 = new Region(addressOffset + offset, PRI_REQUEST.length, 3);
+
+        // HEADERS
+        offset += PRI_REQUEST.length + 10;
+        Http2HeadersFW headers = writeHeaders(buf, offset);
+        Region r2 = new Region(addressOffset + offset, headers.sizeof(), 3);
+
+        // DATA
+        offset = headers.limit() + 10;
+        Http2DataFW data = writeData(buf, offset);
+        Region r3 = new Region(addressOffset + offset, data.sizeof(), 3);
+
+        // SETTINGS
+        offset = data.limit() + 10;
+        Http2SettingsFW settings = writeSettings(buf, offset);
+        Region r4 = new Region(addressOffset + offset, settings.sizeof(), 3);
+
+        Http2Decoder decoder = new Http2Decoder(memoryManager, DefaultDirectBufferBuilder::new,
+                Settings.DEFAULT_MAX_FRAME_SIZE, this::frame, null, null);
+
+        MutableDirectBuffer regionBuf = new UnsafeBuffer(new byte[4096]);
+        ListFW<RegionFW> regionRO = new ListFW.Builder<>(new RegionFW.Builder(), new RegionFW())
+                .wrap(regionBuf, 0, regionBuf.capacity())
+                .item(m -> m.address(r1.address).length(r1.length).streamId(r1.streamId))
+                .item(m -> m.address(r2.address).length(r2.length).streamId(r2.streamId))
+                .item(m -> m.address(r3.address).length(r3.length).streamId(r3.streamId))
+                .item(m -> m.address(r4.address).length(r4.length).streamId(r4.streamId))
+                .build();
+        decoder.decode(regionRO);
+
+        assertEquals(3, frameCount);
+    }
 
     @Test
     public void decode()
     {
         MemoryManager memoryManager = memoryManager();
         MutableDirectBuffer buf = new UnsafeBuffer(new byte[0]);
-
         long addressOffset = memoryManager.acquire(32768);
         long resolvedOffset = memoryManager.resolve(addressOffset);
         buf.wrap(resolvedOffset, 32768);
+
+        // PRI
         int offset = 0;
         buf.putBytes(offset, PRI_REQUEST);
         offset += PRI_REQUEST.length;
 
-        Http2HeadersFW headers = new Http2HeadersFW.Builder()
+        // HEADERS
+        Http2HeadersFW headers = writeHeaders(buf, offset);
+        offset = headers.limit();
+
+        // DATA
+        Http2DataFW data = writeData(buf, offset);
+        offset = data.limit();
+
+        // SETTINGS
+        Http2SettingsFW settings = writeSettings(buf, offset);
+        offset = settings.limit();
+
+        // Test with all region length combinations
+        for(int regionLength=0; regionLength < offset; regionLength++)
+        {
+            frameCount = 0;
+
+            List<Integer> addressList = IntStream.range(0, offset).boxed().collect(Collectors.toList());
+            List<Region> regions = batches(addressList, regionLength + 1)
+                    .stream()
+                    .map(l -> new Region(addressOffset + l.get(0), l.size(), 3))
+                    .collect(Collectors.toList());
+
+            Http2Decoder decoder = new Http2Decoder(memoryManager, DefaultDirectBufferBuilder::new,
+                    Settings.DEFAULT_MAX_FRAME_SIZE, this::frame, null, null);
+
+            List<List<Region>> regionBatches = batches(regions, 2);
+            for(List<Region> regionBatch : regionBatches)
+            {
+                MutableDirectBuffer regionBuf = new UnsafeBuffer(new byte[4096]);
+                ListFW.Builder<RegionFW.Builder, RegionFW> list =
+                        new ListFW.Builder<>(new RegionFW.Builder(), new RegionFW())
+                                .wrap(regionBuf, 0, regionBuf.capacity());
+                list.iterate(regionBatch, r -> list.item(b -> b.address(r.address).length(r.length).streamId(r.streamId)));
+                ListFW<RegionFW> regionRO = list.build();
+                decoder.decode(regionRO);
+            }
+
+            assertEquals(3, frameCount);
+        }
+    }
+
+    private Http2DataFW writeData(MutableDirectBuffer buf, int offset)
+    {
+        byte[] bytes = "123456789012345678901234567890".getBytes();
+        DirectBuffer payload = new UnsafeBuffer(bytes);
+        return new Http2DataFW.Builder()
+                .wrap(buf, offset, buf.capacity())
+                .streamId(5)
+                .payload(payload)
+                .build();
+    }
+
+    private Http2HeadersFW writeHeaders(MutableDirectBuffer buf, int offset)
+    {
+        return new Http2HeadersFW.Builder()
                 .wrap(buf, offset, buf.capacity())   // non-zero offset
                 .header(h -> h.indexed(2))      // :method: GET
                 .header(h -> h.indexed(6))      // :scheme: http
@@ -71,45 +172,18 @@ public class Http2DecoderTest {
                 .endHeaders()
                 .streamId(3)
                 .build();
-        offset = headers.limit();
-
-        byte[] bytes = "123456789012345678901234567890".getBytes();
-        DirectBuffer payload = new UnsafeBuffer(bytes);
-        Http2DataFW data = new Http2DataFW.Builder()
-                .wrap(buf, offset, buf.capacity())
-                .streamId(5)
-                .payload(payload)
-                .build();
-        offset = data.limit();
-
-        Http2SettingsFW settings = new Http2SettingsFW.Builder()
-                .wrap(buf, offset, buf.capacity())
-                .ack()
-                .build();
-        offset = settings.limit();
-
-        int offset1 = offset;
-        int p = 2;
-        int fullChunks = (offset - 1) / p;
-        List<Region> regions = IntStream.range(0, fullChunks + 1)
-                                        .mapToObj(n -> new Region(n, n == fullChunks ? offset1%p : p, 3))
-                                        .collect(Collectors.toList());
-
-        MutableDirectBuffer regionBuf = new UnsafeBuffer(new byte[4096]);
-
-        ListFW.Builder<RegionFW.Builder, RegionFW> list = new ListFW.Builder<>(new RegionFW.Builder(), new RegionFW())
-                .wrap(regionBuf, 0, regionBuf.capacity());
-        regions.forEach(r -> list.item(b -> b.address(addressOffset + 2 * r.address).length(r.length).streamId(r.streamId)));
-        ListFW<RegionFW> region = list.build();
-
-        Http2Decoder decoder = new Http2Decoder(memoryManager, DefaultDirectBufferBuilder::new,
-                Settings.DEFAULT_MAX_FRAME_SIZE, this::frame, null, null);
-        decoder.decode(region);
-
-        assertEquals(3, frameCount);
     }
 
-    void frame(Http2FrameFW frame) {
+    private Http2SettingsFW writeSettings(MutableDirectBuffer buf, int offset)
+    {
+        return new Http2SettingsFW.Builder()
+                    .wrap(buf, offset, buf.capacity())
+                    .ack()
+                    .build();
+    }
+
+    private void frame(Http2FrameFW frame)
+    {
         switch (frameCount)
         {
             case 0:
@@ -122,7 +196,7 @@ public class Http2DecoderTest {
                 assertEquals(SETTINGS, frame.type());
                 break;
             default:
-                throw new IllegalStateException();
+                throw new IllegalStateException("Illegal frame count = " + frameCount);
         }
         frameCount++;
     }
@@ -140,6 +214,14 @@ public class Http2DecoderTest {
         return new DefaultMemoryManager(layout);
     }
 
+    private static <T> List<List<T>> batches(List<T> source, int length) {
+        int size = source.size();
+        int fullChunks = (size - 1) / length;
+        return IntStream.range(0, fullChunks + 1)
+                        .mapToObj(n -> source.subList(n * length, n == fullChunks ? size : (n + 1) * length))
+                        .collect(Collectors.toList());
+    }
+
     private static final class Region
     {
         final long address;
@@ -155,7 +237,7 @@ public class Http2DecoderTest {
 
         public String toString()
         {
-            return String.format("[%d - %d]", address, length);
+            return String.format("[address=%d length=%d]", address, length);
         }
     }
 }
