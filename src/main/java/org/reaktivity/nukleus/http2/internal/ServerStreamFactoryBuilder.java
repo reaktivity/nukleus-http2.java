0f3f834d2dc5b97fd7f20b394a9c938c154a0cf3
John Fallows <john.fallows@kaazing.com>
2018-07-30 15:46:58 -0700

    Update dependency versions

--- src/main/java/org/reaktivity/nukleus/http2/internal/ServerStreamFactoryBuilder.java
+++ src/main/java/org/reaktivity/nukleus/http2/internal/ServerStreamFactoryBuilder.java
@@ -104,7 +104,7 @@ public final class ServerStreamFactoryBuilder implements StreamFactoryBuilder
     }
 
     @Override
-    public ServerStreamFactoryBuilder setCorrelationIdSupplier(
+    public ServerStreamFactoryBuilder setTargetCorrelationIdSupplier(
         LongSupplier supplyCorrelationId)
     {
         this.supplyCorrelationId = supplyCorrelationId;
